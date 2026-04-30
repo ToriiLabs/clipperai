d86ff79c4ae5af04eda6a83a3b135e3e25c3d69e
Redtails74 <pep4474@me.com>
2026-04-30 18:35:00 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
 
+def create_app():
+    app = Flask(__name__,
+                static_url_path='/static',
+                static_folder='../static',
+                template_folder='../templates')
+    
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    
+    from .routes import bp as routes_bp
+    app.register_blueprint(routes_bp)
+    
+    return app
