fb0377253ed1e64b6460629d8df11af32ca2d6da
Redtails74 <pep4474@me.com>
2026-04-30 18:35:24 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    MAX_HISTORY = 10
+    MODEL_PATH = 'google/flan-t5-large'
+    HUGGINGFACE_API_KEY = os.environ.get("HUGGINGFACE_API_KEY", "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")
