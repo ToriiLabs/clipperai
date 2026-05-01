490c064b3c2ba415ed55ef64275c6342d338ac5f
Redtails74 <pep4474@me.com>
2026-04-30 20:16:03 -0400

    Update config.py for database and model settings

--- app/config.py
+++ app/config.py
@@ -1,6 +1,11 @@
 import os
+from datetime import timedelta
 
 class Config:
-    MAX_HISTORY = 10
+    SECRET_KEY = os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///clipperai.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    MAX_HISTORY = 20
     MODEL_PATH = 'google/flan-t5-large'
-    HUGGINGFACE_API_KEY = os.environ.get("HUGGINGFACE_API_KEY", "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")
+    HUGGINGFACE_API_KEY = os.environ.get("HUGGINGFACE_API_KEY")
