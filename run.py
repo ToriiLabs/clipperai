050058193b89dda585e97c8cefc4ea983c622c81
Redtails74 <pep4474@me.com>
2026-04-30 21:25:01 -0400

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,8 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    os.makedirs("data", exist_ok=True)  # For ChromaDB
-    app.run(debug=True)
+    os.makedirs("data", exist_ok=True)
+    os.makedirs("data/uploads", exist_ok=True)
+    
+    # Important for Codespaces
+    app.run(host='0.0.0.0', port=5000, debug=True)
